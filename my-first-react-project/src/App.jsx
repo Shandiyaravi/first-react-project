77f63a69820fe7a3c5e9de57191875ba6ffaf8cb
Shandiya <141755437+Shandiyaravi@users.noreply.github.com>
2023-12-20 14:14:20 +0100

    first commit

---
+++ my-first-react-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Food from './Food.jsx';
+
+export default function App() {
+  
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+    </>
+      
+  );
+}
+
---
+++ my-first-react-project/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer(){
+ return(
+
+    <footer>
+        <p>&copy; {new Date().getFullYear()} Made by Shandiya Ravichandran</p>
+    </footer>
+ );
+
+}
